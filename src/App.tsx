72b75a2c336923ce1ab8576ccca6a43e4f3f4af0
codecrusherindia <drbrc.connect@gmail.com>
2025-12-18 21:44:40 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { LanguageProvider } from "@/contexts/LanguageContext";
+import { LanguageProvider } from "@/context/LanguageContext"; // <--- Import this
+
 import Index from "./pages/Index";
 import Chatbot from "./pages/Chatbot";
 import Features from "./pages/Features";
@@ -24,8 +25,8 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <LanguageProvider>
-      <TooltipProvider>
+    <TooltipProvider>
+      <LanguageProvider> {/* <--- Wrap your app here */}
         <Toaster />
         <Sonner />
         <BrowserRouter>
@@ -48,8 +49,8 @@ const App = () => (
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
-      </TooltipProvider>
-    </LanguageProvider>
+      </LanguageProvider> {/* <--- Close the wrapper here */}
+    </TooltipProvider>
   </QueryClientProvider>
 );
 
